96f4d6c7f88514aeb903c3aa51ddef1edb28788a
Karl Morris <karl.morris@temple.edu>
2021-10-12 11:47:56 -0400

    Interface implementation

---
+++ app/src/main/java/edu/temple/fragmentapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package edu.temple.fragmentapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), MyFragment.MyInterface {
+
+    lateinit var fragment1: MyFragment
+    lateinit var displayFragment: DisplayFragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fragment1 = MyFragment.getInstance(1)
+        displayFragment = DisplayFragment()
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.container1, fragment1)
+            .add(R.id.container2, displayFragment)
+            .commit()
+
+        //fragment1.changeColor(4)
+
+
+        findViewById<Button>(R.id.changeColorButton).setOnClickListener{
+            //fragment1.changeColor(3)
+            displayFragment.changeColor("blue")
+        }
+    }
+
+    fun myFunction() {
+
+    }
+
+    override fun colorSelected(color: String) {
+        displayFragment.changeColor(color)
+    }
+}
---
+++ app/src/main/java/edu/temple/fragmentapp/MyFragment.kt
@@ -0,0 +1,76 @@
+package edu.temple.fragmentapp
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+
+class MyFragment : Fragment() {
+
+    lateinit var spinner: Spinner
+    lateinit var layout: View
+
+    var startupIndex : Int? = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startupIndex = arguments?.getInt("startupIndex", 0)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        layout = inflater.inflate(R.layout.fragment_my, container, false)
+
+        spinner = layout.findViewById(R.id.spinner)
+
+        spinner.adapter = ArrayAdapter(requireContext()
+            , android.R.layout.simple_spinner_dropdown_item
+            , requireContext().resources.getStringArray(R.array.colors))
+
+        spinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                //layout.setBackgroundColor(Color.parseColor(p0?.getItemAtPosition(p2).toString()))
+                (requireActivity() as MyInterface).colorSelected(p0?.getItemAtPosition(p2).toString())
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+        return layout
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        changeColor(startupIndex)
+    }
+
+    fun changeColor(index: Int?) {
+        if (index != null) {
+            spinner.setSelection(index)
+        }
+    }
+
+    companion object {
+        fun getInstance (index : Int) : MyFragment {
+            val fragment = MyFragment()
+            val bundle: Bundle = Bundle()
+            bundle.putInt("startupIndex", index)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+    interface MyInterface {
+        fun colorSelected(color: String)
+    }
+
+}
